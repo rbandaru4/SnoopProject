6eee519d7db0db599f0a0e2ca28ac574f2ce5d6c
rajeswaribandarupalli <60739096+rbandaru4@users.noreply.github.com>
2025-06-12 09:20:18 +0100

    Update Transaction.java

--- transactions-api-tests/src/test/java/models/Transaction.java
+++ transactions-api-tests/src/test/java/models/Transaction.java
@@ -3,14 +3,15 @@ package models;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
+
 
 import java.time.OffsetDateTime;
 
-@Data
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Transaction {
-    @JsonProperty("transactionId")
+
+     @JsonProperty("transactionId")
     private String transactionId;
 
     @JsonProperty("amount")
@@ -40,4 +41,84 @@ public class Transaction {
 
     @JsonProperty("description")
     private String description;
+    
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public void setMerchantName(String merchantName) {
+        this.merchantName = merchantName;
+    }
+
+    public void setTimestamp(OffsetDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setSubType(String subType) {
+        this.subType = subType;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public String getMerchantName() {
+        return merchantName;
+    }
+
+    public OffsetDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getSubType() {
+        return subType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
